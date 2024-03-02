e7c864d1c9415e4f516d718de450850d8ede9b93
egemertgulderen <egemertgulderen@gmail.com>
2024-03-02 14:52:02 +0300

    updated character movements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5f; // Adjust this to control movement speed
+    private bool isMoving = false;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!isMoving) // Only allow movement if not already moving
+        {
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+            float verticalInput = Input.GetAxisRaw("Vertical");
+
+            if (horizontalInput != 0 && verticalInput == 0)
+            {
+                MovePlayer(new Vector2(horizontalInput, 0));
+            }
+            else if (verticalInput != 0 && horizontalInput == 0)
+            {
+                MovePlayer(new Vector2(0, verticalInput));
+            }
+        }
     }
+
+    void MovePlayer(Vector2 direction)
+    {
+        isMoving = true;
+
+        Vector2 targetPosition = (Vector2)transform.position + direction;
+        StartCoroutine(MoveCoroutine(targetPosition));
+    }
+
+    System.Collections.IEnumerator MoveCoroutine(Vector2 targetPosition)
+    {
+        while (Vector2.Distance(transform.position, targetPosition) > 0.01f)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        isMoving = false;
+    }
+
 }
